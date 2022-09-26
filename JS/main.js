df9b1c471c217243242d1c1e2e5d07f5f2557799
Suraj Gond <surajgond07@gmail.com>
2022-09-26 22:36:26 +0530

    Navbar build.

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.nav').addClass("sticky");
+        } else {
+            $('.nav').removeClass("sticky");
+        }
+    })
+});
