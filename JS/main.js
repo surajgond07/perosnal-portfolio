c5da4184ed3c50592438fd0d0f20ca42ecfe2682
Suraj Gond <surajgond07@gmail.com>
2022-12-07 16:23:59 +0530

    Teams section build.

--- JS/main.js
+++ JS/main.js
@@ -11,8 +11,31 @@ $(document).ready(function() {
 
     // toogle menu /nvabar script 
     $('.nav__toggler').click(function() {
-        $('.navbar__menu').toggleClass("active");
-        // for closing menu button x
-        $('.nav__toggler i').toggleClass("active");
-    })
+            $('.navbar__menu').toggleClass("active");
+            // for closing menu button x
+            $('.nav__toggler i').toggleClass("active");
+        })
+        // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
 });
