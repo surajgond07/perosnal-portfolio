08d54df87ca6c082da9947d4da2356ce90597df1
Suraj Gond <surajgond07@gmail.com>
2022-12-09 16:39:51 +0530

    Scroll -up build.

--- JS/main.js
+++ JS/main.js
@@ -7,10 +7,20 @@ $(document).ready(function() {
         } else {
             $('.nav').removeClass("sticky");
         }
+        // scroll up btn 
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
 
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
 
     });
 
+    // slide-up script 
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+    });
 
     // toogle menu /navabar script 
     $('.nav__toggler').click(function() {
